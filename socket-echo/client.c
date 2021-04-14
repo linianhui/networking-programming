2ba2719eb0134aeaf244ae943f06461ba6fecd1c
lnh <lnhdyx@outlook.com>
2021-04-14 17:00:27 +0800

    use send and recv

--- socket-echo/client.c
+++ socket-echo/client.c
@@ -12,21 +12,19 @@
 
 int main(int argc, char **argv)
 {
-    int connect_fd;
+    int connect_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     struct sockaddr_in server_address;
-
-    connect_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     bzero(&server_address, sizeof server_address);
-
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(SERVER_PORT);
-
     inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
     printf("client srart\n");
 
     connect(connect_fd, (struct sockaddr *)&server_address, sizeof(server_address));
-    printf("cconnected to server 127.0.0.1:12345 ...\n");
+    char *server_ip = inet_ntoa(server_address.sin_addr);
+    in_port_t server_port = server_address.sin_port;
+    printf("cconnected to server %s:%d ...\n", server_ip, server_port);
 
     char read_buffer[BUFFER_SIZE];
     char write_buffer[BUFFER_SIZE];
@@ -35,8 +33,8 @@ int main(int argc, char **argv)
         bzero(read_buffer, BUFFER_SIZE);
         bzero(write_buffer, BUFFER_SIZE);
         fgets(write_buffer, BUFFER_SIZE, stdin);
-        write(connect_fd, write_buffer, strlen(write_buffer) + 1);
-        read(connect_fd, read_buffer, BUFFER_SIZE);
+        send(connect_fd, write_buffer, strlen(write_buffer) + 1, 0);
+        recv(connect_fd, read_buffer, BUFFER_SIZE, 0);
         printf("server echo:\n%s\n", read_buffer);
     }
     close(connect_fd);
--- socket-echo/server.c
+++ socket-echo/server.c
@@ -23,10 +23,10 @@ void echo(int connect_fd, struct sockaddr_in *client_address)
     printf("server pid=%d handler\n", pid);
 
     int read_size;
-    while ((read_size = read(connect_fd, buffer, sizeof(buffer))) != 0)
+    while ((read_size = recv(connect_fd, buffer, sizeof(buffer), 0)) != 0)
     {
         printf("client %s:%d send\n%s\n", client_ip, client_port, buffer);
-        if (write(connect_fd, buffer, read_size + 1) > 0)
+        if (send(connect_fd, buffer, read_size + 1, 0) > 0)
         {
             printf("server pid=%d echo:\n%s\n", pid, buffer);
         }
@@ -36,21 +36,18 @@ void echo(int connect_fd, struct sockaddr_in *client_address)
 
 int main()
 {
-    struct sockaddr_in server_address;
-
-    bzero(&server_address, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htons(INADDR_ANY);
-    server_address.sin_port = htons(LISTEN_PORT);
-
-    int listen_fd;
-
-    if ((listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    int listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listen_fd < 0)
     {
         perror("socket error");
         return 1;
     }
 
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htons(INADDR_ANY);
+    server_address.sin_port = htons(LISTEN_PORT);
     if (bind(listen_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
         perror("bind error");
@@ -62,8 +59,9 @@ int main()
         perror("listen error");
         return 3;
     }
+
     pid_t pid = getpid();
-    printf("server srart pid=%d\nwaiting for client connect...\n", pid);
+    printf("server srart pid=%d port=%d \nwaiting for client connect...\n", pid, LISTEN_PORT);
 
     int connect_fd;
     struct sockaddr_in client_address;
@@ -76,8 +74,7 @@ int main()
             perror("accept error");
             return 4;
         }
-        int childPid;
-        if ((childPid = fork()) == 0)
+        if (fork() == 0)
         {
             close(listen_fd);
             echo(connect_fd, &client_address);
