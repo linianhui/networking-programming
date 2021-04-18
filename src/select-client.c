3f9b4ae149100fcfb7515a8e49852b1db844fc2d
lnh <lnhdyx@outlook.com>
2021-04-18 10:46:22 +0800

    Update select-client.c

--- src/select-client.c
+++ src/select-client.c
@@ -2,10 +2,6 @@
 
 void cli(FILE *input, int connect_fd)
 {
-    int input_fd = fileno(input);
-    int max_fd;
-    fd_set read_fd_set;
-
     char recv_buf[BUFFER_SIZE];
     char send_buf[BUFFER_SIZE];
     char local_prompt[PROMPT_SIZE];
@@ -16,15 +12,20 @@ void cli(FILE *input, int connect_fd)
     get_local_prompt(connect_fd, local_prompt);
     printf_flush("%s ", local_prompt);
 
+    int input_fd = fileno(input);
+    int max_fd = imax(input_fd, connect_fd) + 1;
+    fd_set read_fd_set;
+
     while (1)
     {
         // 每次都需要重新设置，因为select返回时会重置read_fd_set
         FD_ZERO(&read_fd_set);
         FD_SET(input_fd, &read_fd_set);
         FD_SET(connect_fd, &read_fd_set);
-        max_fd = imax(input_fd, connect_fd) + 1;
+
         // 获取可读的fd，阻塞
         select(max_fd, &read_fd_set, NULL, NULL, NULL);
+
         // 读取用户输入，非阻塞
         if (FD_ISSET(input_fd, &read_fd_set))
         {
